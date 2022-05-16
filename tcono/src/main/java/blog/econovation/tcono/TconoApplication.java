88c43e2e153171dbd79ee7785cdb9ccfceb3cedc
BlackBean99 <54030889+BlackBean99@users.noreply.github.com>
2022-05-16 15:40:22 +0900

    feat : Spring 개발환경 구축

---
+++ tcono/src/main/java/blog/econovation/tcono/TconoApplication.java
@@ -0,0 +1,13 @@
+package blog.econovation.tcono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TconoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TconoApplication.class, args);
+	}
+
+}
