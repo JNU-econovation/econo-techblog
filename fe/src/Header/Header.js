a2497c28610a3a3c883746880ffb01e2cb88c789
2yunseong <dbsdltjd3701@naver.com>
2022-05-21 01:23:07 +0900

    feat : Header 컴포넌트 초기 HTML 태그 레이아웃 작성

---
+++ fe/src/Header/Header.js
@@ -0,0 +1,15 @@
+import test_logo from './img/test_logo.png'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img alt="econovation logo" src={test_logo}/>
+            <div className="header-nav-button-box">
+
+            </div>
+            <div className="header-right-box">
+                
+            </div>
+        </div>
+    )
+}
