06c8c11a9d9b8513fc00b7f3d8c75b4b6879bd04
2yunseong <dbsdltjd3701@naver.com>
2022-05-21 02:51:48 +0900

    feat : NavItem 컴포넌트 적용

--- fe/src/Header/Header.js
+++ fe/src/Header/Header.js
@@ -1,16 +1,20 @@
 import LoginButton from './components/LoginButton';
+import NavItem from './components/NavItem';
 import test_logo from './img/test_logo.png';
 import './Header.css';
+
 
 const Header = () => {
     return (
         <div className="header">
             <img alt="econovation logo" src={test_logo}/>
             <div className="header-nav-button-box">
-                
+                <NavItem />
+                <NavItem />
+                <NavItem />
+                <NavItem />
             </div>
             <div className="header-right-box">
-                searchbar
                 <LoginButton />
             </div>
         </div>
