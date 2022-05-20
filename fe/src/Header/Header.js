d8a5c5587b1baed33414d0cfa11e6099f3064ea5
2yunseong <dbsdltjd3701@naver.com>
2022-05-21 02:16:57 +0900

    feat : css import 및 컴포넌트 export

--- fe/src/Header/Header.js
+++ fe/src/Header/Header.js
@@ -1,15 +1,19 @@
-import test_logo from './img/test_logo.png'
+import test_logo from './img/test_logo.png';
+import './Header.css';
 
 const Header = () => {
     return (
         <div className="header">
             <img alt="econovation logo" src={test_logo}/>
             <div className="header-nav-button-box">
-
+                
             </div>
             <div className="header-right-box">
+                searchbar
                 
             </div>
         </div>
     )
 }
+
+export default Header;
