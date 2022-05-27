b7d094581ed3332ee2f86f303dcd6f2d53d5963d
ryeeun <dmsdl7531@naver.com>
2022-05-27 19:32:52 +0900

    feat : Footer 레이아웃 구현

---
+++ fe/src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="footerbox">
+                <p className="footertext">Copyright 2022 ECONOVATION</p>
+                <p className="footertext">All rights reserved</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
