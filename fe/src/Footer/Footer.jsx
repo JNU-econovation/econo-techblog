87a97cf08703cf8b87f980bc3700c4d8802c0b8c
ryeeun <dmsdl7531@naver.com>
2022-05-27 20:03:12 +0900

    style : ESLint 문법에 맞게 수정

--- fe/src/Footer/Footer.jsx
+++ fe/src/Footer/Footer.jsx
@@ -1,14 +1,15 @@
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
-const Footer = () => {
-    return(
-        <div className="footer">
-            <div className="footerbox">
-                <p className="footertext">Copyright 2022 ECONOVATION</p>
-                <p className="footertext">All rights reserved</p>
-            </div>
-        </div>
-    );
-}
+const Footer = function () {
+  return (
+    <div className="footer">
+      <div className="footerbox">
+        <p className="footertext">Copyright 2022 ECONOVATION</p>
+        <p className="footertext">All rights reserved</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
