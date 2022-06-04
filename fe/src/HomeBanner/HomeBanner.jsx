04a18572b8c0a97098be85c2ea371765f7d73c8a
2yunseong <dbsdltjd3701@naver.com>
2022-06-04 17:52:08 +0900

    feat : Homebanner 페이지 기본 텍스트 추가

---
+++ fe/src/HomeBanner/HomeBanner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomeBanner.css';
+
+function HomeBanner() {
+  return (
+    <div className="home-banner">
+      <div className="home-banner-text">
+        <h1>에코노베이션 T-ECONO</h1>
+        <p>
+          에코노베이션 기술 블로그 T-econo입니다. 문제 해결, 에코노베이션의 행사
+          등 에코노 베이션의 다양한 소식을 접할 수 있습니다.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HomeBanner;
