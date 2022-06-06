2152d6dcf4cdb5341efb9aee4c05d9740920ebe8
BlackBean99 <54030889+BlackBean99@users.noreply.github.com>
2022-06-06 15:02:59 +0900

    feat : Controller Env

---
+++ tcono/src/main/java/blog/econovation/tcono/domain/post/postController.java
@@ -0,0 +1,8 @@
+package blog.econovation.tcono.domain.post;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class postController {
+
+}
---
+++ tcono/src/main/java/blog/econovation/tcono/domain/user/UserController.java
@@ -0,0 +1,7 @@
+package blog.econovation.tcono.domain.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
