e406b3850521646deaa36d89ab39dba0adac8d62
2yunseong <dbsdltjd3701@naver.com>
2022-06-27 17:04:37 +0900

    feat : 회원가입버튼 디자인을 구현한다

---
+++ fe/src/SignUp/components/SignUpBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../css/SignUpBtn.css';
+
+function SignUpBtn() {
+  return (
+    <button type="submit" className="sign-up-button">
+      가입하기
+    </button>
+  );
+}
+
+export default SignUpBtn;
