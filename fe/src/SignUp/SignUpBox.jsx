d713ed25ed801a5043b4e4b06dcadb0bdf40cd04
2yunseong <dbsdltjd3701@naver.com>
2022-06-28 22:33:17 +0900

    feat : 필요한 props 추가

--- fe/src/SignUp/SignUpBox.jsx
+++ fe/src/SignUp/SignUpBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardinalSelectBox from './components/CardinalSelectBox';
 
 import InputElement from './components/InputElement';
@@ -6,16 +6,38 @@ import SignUpBtn from './components/SignUpBtn';
 import './css/SignUpBox.css';
 
 function SignUpBox() {
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
   return (
     <div className="sign-up-box">
       <h3 className="sign-up-box-title">Sign Up</h3>
-      <InputElement placeHolder="이메일" classNames="input-email" />
-      <InputElement placeHolder="비밀번호" classNames="input-password" />
+      <InputElement
+        placeHolder="이메일"
+        classNames="input-email"
+        setInput={setEmail}
+        inputValue={email}
+      />
+      <span>이메일 인증하기</span>
+      <InputElement
+        placeHolder="비밀번호"
+        classNames="input-password"
+        setInput={setPassword}
+        inputValue={password}
+      />
       <InputElement
         placeHolder="비밀번호 확인"
         classNames="input-password-confirm"
+        setInput={setRePassword}
+        inputValue={rePassword}
       />
-      <InputElement placeHolder="이름" classNames="input-name" />
+      <InputElement
+        placeHolder="이름"
+        classNames="input-name"
+        setInput={setName}
+        inputValue={name}
+      />
       <CardinalSelectBox />
       <SignUpBtn />
     </div>
