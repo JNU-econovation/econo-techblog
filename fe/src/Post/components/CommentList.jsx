34895fb1cd2119dabba6eb0b07f1db00ee3c31fb
ryeeun <dmsdl7531@naver.com>
2022-06-30 16:18:37 +0900

    refactor : comment list 렌더링 코드 수정

--- fe/src/Post/components/CommentList.jsx
+++ fe/src/Post/components/CommentList.jsx
@@ -4,10 +4,28 @@ import '../css/CommentList.css';
 
 import Comment from './Comment';
 
+const result = [
+  {
+    id: 1,
+    name: '김수민',
+    desc: '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
+  },
+  {
+    id: 2,
+    name: '김수민',
+    desc: '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
+  },
+  {
+    id: 3,
+    name: '김수민',
+    desc: '안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한 것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건 어떤 방식으로 걸러내는 건가요?',
+  },
+];
+
 const CommentList = function () {
   return (
     <div className="comment-list">
-      <span>댓글 3개</span>
+      <span className="comment-num">{`댓글 ${result.length}개`}</span>
       <div className="comment">
         <div className="writer-img" />
         <div className="comment-info">
@@ -15,12 +33,9 @@ const CommentList = function () {
           <input />
         </div>
       </div>
-      <Comment
-        name="김수민"
-        desc="안녕하세요 궁금한점이 있습니다! 만약에 사용자가 빌린 차를 세차한
-        것이 아니라 구글링이나 다른 자동차를 세차한 이미지를 올린다면 이건
-        어떤 방식으로 걸러내는 건가요?"
-      />
+      {result.map((comment) => (
+        <Comment key={comment.id} name={comment.name} desc={comment.desc} />
+      ))}
     </div>
   );
 };
