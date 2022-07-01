8668b48dc2513dddd6984de06c34890ed94f194c
2yunseong <dbsdltjd3701@naver.com>
2022-07-01 11:49:34 +0900

    feat : check 컴포넌트 구현

---
+++ fe/src/SignUp/components/CheckElement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CheckElement() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>개인정보 동의합니다.</p>
+    </div>
+  );
+}
+
+export default CheckElement;
