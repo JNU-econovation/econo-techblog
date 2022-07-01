4d922bcbe8015a4a3e4f7d851643e2229bb3726b
BlackBean99 <54030889+BlackBean99@users.noreply.github.com>
2022-07-01 17:42:59 +0900

    test : updateUser

--- tcono/src/test/java/blog/econovation/tcono/user/UserResolverTest.java
+++ tcono/src/test/java/blog/econovation/tcono/user/UserResolverTest.java
@@ -79,7 +79,7 @@ public class UserResolverTest {
         User savedUser = userRepository.save(user);
 
     //        when : act
-    User findUser = userRepository.findById(user.getId())
+        User findUser = userRepository.findById(user.getId())
                     .orElseThrow(()-> new IllegalArgumentException("Wrong UserId:< " + user.getId() + ">"));
 //    then
         assertThat(findUser.getName()).isEqualTo("이서현");
@@ -110,17 +110,36 @@ public class UserResolverTest {
     //    회원가입 인증 테스트
     //    랜덤 키값을 형성하기 때문에 함수가 호출됐는지를 테스트한다.
     @Test(expected = NullPointerException.class)
-    Transactional
+    @Transactional
     public void confirmEmailTest(){
         EmailMutationResolver emailMutationResolverMocks = mock(EmailMutationResolver.class);
         doThrow(NullPointerException.class)
                 .when(emailMutationResolverMocks);
         verify(emailMutationResolverMocks,times(1));
     }
-g
 //  로그인 테스트
 
 //  회원 정보 수정 테스트
+    @Test
+    @Transactional
+    public void updateUser(){
+        User user1 = User.builder()
+                .userEmail("Test1@gmail.com")
+                .password("123")
+                .year(22)
+                .userName("이서현1");
+
+        User user2 = User.builder()
+                .userEmail("Test2@gmail.com")
+                .password("12345")
+                .year(20)
+                .userName("이서현2");
+        userRepositort.save(user1);
+        userRepository.update(user2);
+
+        User findUser = userRepository.findByName("이서현1");
+        asseertThat(findUser.getName()).isEqualTo(user2.getName());
+    }
 
 //  회원정보 삭제 테스트
 }
