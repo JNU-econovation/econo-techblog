46fff940d97e0a445294c08657a6c538cdd4647a
2yunseong <dbsdltjd3701@naver.com>
2022-07-01 11:47:57 +0900

    feat : 회원가입 버튼 스켈레톤 코드 작성

--- fe/src/SignUp/components/SignUpBtn.jsx
+++ fe/src/SignUp/components/SignUpBtn.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 import '../css/SignUpBtn.css';
 
 function SignUpBtn() {
+  const onClick = () => {
+    // 이메일 인증 완료 로직
+    // 모두 입력되었는지 검증하는 로직
+  };
+
   return (
     <div className="box-element">
-      <button type="submit">가입하기</button>
+      <button type="submit" onClick={onClick}>
+        가입하기
+      </button>
     </div>
   );
 }
