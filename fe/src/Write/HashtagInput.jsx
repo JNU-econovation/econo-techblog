f961741f15f538b8ee636bb0045fefe002c77acb
2yunseong <dbsdltjd3701@naver.com>
2022-07-07 16:33:25 +0900

    feat : 태그 중복 입력 방지 로직 추가

--- fe/src/Write/HashtagInput.jsx
+++ fe/src/Write/HashtagInput.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './css/HashtagInput.css';
 
 const SEPERATOR = ',';
+const BACK_SPACE_CODE = 8;
 
 function HashtagInput() {
   const [hashtagList, setHashtagList] = useState([]);
@@ -12,6 +13,8 @@ function HashtagInput() {
     const lastChar = content.charAt(content.length - 1);
     if (lastChar === SEPERATOR) {
       if (inputState === '') {
+        setInputState(() => '');
+      } else if (hashtagList.indexOf(inputState) !== -1) {
         setInputState(() => '');
       } else {
         const newList = [...hashtagList, content.substr(0, content.length - 1)];
@@ -29,6 +32,10 @@ function HashtagInput() {
   const onKeyPress = (e) => {
     if (e.key === 'Enter') {
       if (inputState === '') return;
+      if (hashtagList.indexOf(inputState) !== -1) {
+        setInputState(() => '');
+        return;
+      }
       const newList = [...hashtagList, inputState];
       setHashtagList(() => newList);
       setInputState(() => '');
@@ -36,23 +43,37 @@ function HashtagInput() {
   };
 
   const onKeyDown = (e) => {
-    if (inputState === '') {
-      if (e.keyCode === 8) {
+    if (inputState === '') {₩
+      if (e.keyCode === BACK_SPACE_CODE) {
         const newList = hashtagList.splice(0, hashtagList.length - 1);
         setHashtagList(() => newList);
       }
     }
   };
+
+  const onClick = (e) => {
+    const newList = hashtagList.filter((hashtag) => hashtag !== e.target.value);
+    setHashtagList(() => newList);
+  };
   return (
     <div className="hashtag-input-container">
       {hashtagList.map((e) => (
-        <div className="hashtag-element">{`#${e}`}</div>
+        <button
+          type="button"
+          onClick={onClick}
+          className="hashtag-element"
+          value={e}
+        >
+          {`#${e}`}
+        </button>
       ))}
       <input
+        className="hashtag-input"
         onChange={onChange}
         onKeyDown={onKeyDown}
         onKeyPress={onKeyPress}
         value={inputState}
+        placeholder="태그를 입력하세요..."
       />
     </div>
   );
