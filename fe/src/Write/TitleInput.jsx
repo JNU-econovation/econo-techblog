92cee025fc23fd63f18c787adf1d20b6fa5ab045
2yunseong <dbsdltjd3701@naver.com>
2022-07-07 16:45:15 +0900

    feat : title input 스켈레톤 코드

---
+++ fe/src/Write/TitleInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './css/TitleInput.css';
+
+function TitleInput() {
+  return (
+    <div className="title-input-container">
+      <input className="title-input" placeholder="제목을 입력하세요..." />
+    </div>
+  );
+}
+
+export default TitleInput;
