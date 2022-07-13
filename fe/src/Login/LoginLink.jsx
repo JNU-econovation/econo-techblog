5f51e620c67cc806fda38e567341d80319c542ae
2yunseong <dbsdltjd3701@naver.com>
2022-07-13 20:01:17 +0900

    refactor: BEM 스타일로 css 변경

--- fe/src/Login/LoginLink.jsx
+++ fe/src/Login/LoginLink.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './css/LoginLink.css';
 
 function LoginLink() {
   return (
-    <div className="login-link-container">
-      <div>아이디 찾기</div>
+    <div className="login-link__content">
+      <Link to="/findinfo">아이디 찾기</Link>
       <div>|</div>
-      <div>비밀번호 찾기</div>
+      <Link to="/findinfo">비밀번호 찾기</Link>
     </div>
   );
 }
