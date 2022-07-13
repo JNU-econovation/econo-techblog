1078fce38de5ce24befd9fcad73955459051d1b5
2yunseong <dbsdltjd3701@naver.com>
2022-07-13 21:33:17 +0900

    feat : write 페이지 기본 css - grid 로 구성

--- fe/src/Write/Write.jsx
+++ fe/src/Write/Write.jsx
@@ -6,6 +6,7 @@ import { Editor } from '@toast-ui/react-editor';
 import HashtagInput from './HashtagInput';
 import TitleInput from './TitleInput';
 import CategorySelectBox from './CategorySelectBox';
+import './css/Write.css';
 
 function Write() {
   const editorRef = useRef();
@@ -18,17 +19,29 @@ function Write() {
   };
   return (
     <div className="write">
-      <CategorySelectBox />
-      <TitleInput />
-      <HashtagInput />
-      <Editor
-        ref={editorRef}
-        initialValue=""
-        placeholder="여기에 내용을 입력하세요.."
-        previewStyle="vertical"
-        height="80vh"
-      />
-      <button type="submit" onClick={onSubmit}>
+      <h3 className="write-title">게시판 글쓰기</h3>
+      <div className="write__grid">
+        <div className="write-category__title">게시판</div>
+        <CategorySelectBox />
+        <div className="write-category__title">제목</div>
+        <TitleInput />
+        <div className="write-category__title">태그</div>
+        <HashtagInput />
+        <div className="write-category__title">본문</div>
+        <div className="write-editor">
+          <Editor
+            ref={editorRef}
+            initialValue=" "
+            placeholder="여기에 내용을 입력하세요.."
+            previewStyle="vertical"
+            height="100%"
+          />
+        </div>
+      </div>
+      <button className="write-cancel__button" type="button">
+        취소
+      </button>
+      <button className="write-submit__button" type="submit" onClick={onSubmit}>
         작성완료
       </button>
     </div>
