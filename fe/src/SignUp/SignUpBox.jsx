1ae1f7f41b25a1e3935cb7ea3e120deed72163f0
2yunseong <dbsdltjd3701@naver.com>
2022-07-14 22:00:05 +0900

    fix: class name bem 방식으로 변경

--- fe/src/SignUp/SignUpBox.jsx
+++ fe/src/SignUp/SignUpBox.jsx
@@ -33,21 +33,21 @@ function SignUpBox() {
       <h3 className="sign-up-box-title">Sign Up</h3>
       <InputElement
         placeHolder="이메일"
-        classNames="input-email"
+        classNames="sign-up-box-email__input"
         value={email}
         setValue={setEmail}
       />
       <EmailStatus email={email} />
       <InputElement
         placeHolder="비밀번호"
-        classNames="input-password"
+        classNames="sign-up-box-password__input"
         value={password}
         setValue={setPassword}
         type="password"
       />
       <InputElement
         placeHolder="비밀번호 확인"
-        classNames="input-password-confirm"
+        classNames="sign-up-box-password-confirm__input"
         value={confirmPasswd}
         setValue={setconfirmPasswd}
         type="password"
@@ -55,13 +55,15 @@ function SignUpBox() {
       <span>{passwdStatus}</span>
       <InputElement
         placeHolder="이름"
-        classNames="input-name"
+        classNames="sign-up-box-name__input"
         value={name}
         setValue={setName}
       />
-      <CardinalSelectBox />
-      <input type="checkbox" checked={isAgree} onChange={onChange} />
-      개인정보 동의합니다.
+      <CardinalSelectBox classNames="sign-up-box-cardinal__select" />
+      <div className="sign-up-box--agree__container">
+        <input type="checkbox" checked={isAgree} onChange={onChange} />
+        <span>이용약관 및 개인정보 처리방침에 동의합니다.</span>
+      </div>
       <SignUpBtn />
     </div>
   );
