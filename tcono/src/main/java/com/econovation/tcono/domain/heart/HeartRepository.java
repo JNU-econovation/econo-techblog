969be871a4b5c9a23d4d223aca32e7055e295fe8
kssumin <ksoomin25@gmail.com>
2022-07-18 09:02:32 +0900

    fix : repositorty에서 data가 안불러오는 문제 해결

--- tcono/src/main/java/com/econovation/tcono/domain/heart/HeartRepository.java
+++ tcono/src/main/java/com/econovation/tcono/domain/heart/HeartRepository.java
@@ -3,24 +3,29 @@ package com.econovation.tcono.domain.heart;
 import com.econovation.tcono.domain.post.Post;
 import com.econovation.tcono.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
     /**
      * 게시물(post)에 특정 회원이 회원(user)이 좋아요(heart)를 눌렀는지?
-     * @param user
-     * @param post
+     * @param userId
+     * @Param post
      * @return Optional<Post>
      */
-    Optional<Heart>findHeartByUserAndPost(User user, Post post);
+    @Query("select h from Heart h where h.userId=:userId AND h.post=:post")
+    Optional<Heart>findHeartByUserIdAndUserId(@Param("userId") Long userId, @Param("post") Post post);
 
     /**
      * 게시물(post)에 특정 회원(user)이 한 좋아요(heart)를 삭제
-     * @param user
+     * @param userId
      * @param post
      */
-    Optional<Heart> deleteHeartByUserAndPost(User user, Post post);
-
+    @Modifying
+    @Query("delete from Heart h where h.userId=:userId AND h.post=:post")
+    Optional<Heart> deleteHeartByUserIdAndPost(@Param("userId") Long userId,@Param("post") Post post);
 
 }
