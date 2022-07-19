7f7cb94f1ba08c7abe19297ba31daf4f6cf815e9
kssumin <ksoomin25@gmail.com>
2022-07-19 18:24:41 +0900

    refactor : post당 한 user의 views수가 1개로 제한되어 있는 에러 해결

--- tcono/src/main/java/com/econovation/tcono/domain/post/PostQueryResolver.java
+++ tcono/src/main/java/com/econovation/tcono/domain/post/PostQueryResolver.java
@@ -1,8 +1,9 @@
 package com.econovation.tcono.domain.post;
-
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostQueryResolver implements GraphQLQueryResolver {
     private static final String NOT_FOUND_POST_MESSAGE = "해당 페이지가 없습니다.";
+
     private final PostRepository postRepository;
 
     /**
@@ -22,7 +24,7 @@ public class PostQueryResolver implements GraphQLQueryResolver {
 
     @Transactional
     public List<Post> findAllPosts(MainCategory mainCategory) {
-        List<Post> post = postRepository.findByMainCategory(MainCategory.getMainCategory(mainCategory));
+        List<Post> post = postRepository.findAllByMainCategory(MainCategory.getMainCategory(mainCategory));
         return post;
     }
 
@@ -32,11 +34,16 @@ public class PostQueryResolver implements GraphQLQueryResolver {
      * 댓글 같이 보내기
      */
 
-    @Transactional
-    public Post findPostByPosyId(Long postId) {
-        Post post = postRepository.findById(postId)
+    public Post findPostByPostId(Long id) {
+        Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_POST_MESSAGE));
-        post.increaseViews();
+        increaseViews(post);
         return post;
+    }
+
+    @Transactional
+    public int increaseViews(Post post) {
+        postRepository.updateViews(post.getId());
+        return post.getViews();
     }
 }
--- tcono/src/main/java/com/econovation/tcono/domain/post/PostRepository.java
+++ tcono/src/main/java/com/econovation/tcono/domain/post/PostRepository.java
@@ -1,12 +1,15 @@
 package com.econovation.tcono.domain.post;
 
 import com.econovation.tcono.domain.user.User;
+import com.econovation.tcono.web.dto.PostCreateRequestDto;
+import com.econovation.tcono.web.dto.PostCreateResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +23,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("select p from Post p where p.id=:id")
     Optional<Post> findById(@Param("id") Long id);
+
+    @Transactional
+    @Modifying
+    @Query("update Post p set p.views = p.views + 1 where p.id = :id")
+    int updateViews(@Param("id")Long id);
+
 }
