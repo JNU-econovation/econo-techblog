5a1b7ef4ba23109af7aa1c0479ff758000e7f2f1
2yunseong <dbsdltjd3701@naver.com>
2022-07-20 00:01:25 +0900

    feat : MyPage CSS 추가

--- fe/src/MyPage/MyPosts.jsx
+++ fe/src/MyPage/MyPosts.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './css/MyPosts.css';
 
 function MyPosts() {
+  const [type, setType] = useState('like');
+  const onLikeClick = () => {
+    setType(() => 'like');
+  };
+  const onCommentClick = () => {
+    setType(() => 'comment');
+  };
   return (
     <div className="my-posts">
       <h3>내가 쓴 글(0)</h3>
       <div className="my-posts__container">아직 작성한 글이 없습니다.</div>
-      <div className="my-posts-group__nav">좋아요(2) 댓글(5)</div>
+      <div className="my-posts-group__nav">
+        <button
+          className={`my-posts-nav__button ${
+            type === 'like' ? 'nav__button--active' : ''
+          }`}
+          type="button"
+          onClick={onLikeClick}
+        >
+          좋아요(2)
+        </button>
+        <button
+          className={`my-posts-nav__button ${
+            type === 'comment' ? 'nav__button--active' : ''
+          }`}
+          type="button"
+          onClick={onCommentClick}
+        >
+          댓글(5)
+        </button>
+      </div>
       <div className="my-posts-group__container">글들 글들 글들</div>
     </div>
   );
