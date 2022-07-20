7ab25d69eacc4b38b20439d71e2b04dd35e3e02e
2yunseong <dbsdltjd3701@naver.com>
2022-07-20 21:41:17 +0900

    refactor: 서버에 맞게 리팩터링

--- fe/src/SignUp/SignUpBox.jsx
+++ fe/src/SignUp/SignUpBox.jsx
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import CardinalSelectBox from '../components/CardinalSelectBox';
 
+import CardinalSelectBox from '../components/CardinalSelectBox';
 import InputElement from '../components/InputElement';
-import SignUpBtn from './components/SignUpBtn';
 import EmailStatus from './EmailStatus';
 import { isEmptyStatus, isSamePasswd } from './util/VerifyPassword';
 import './css/SignUpBox.css';
 
 function SignUpBox() {
   const [isAgree, setIsAgree] = useState(false);
-  const [email, setEmail] = useState('');
-  const [passwd, setPasswd] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [confirmPasswd, setconfirmPasswd] = useState('');
-  const [name, setName] = useState('');
+  const [userName, setUserName] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [year, setYear] = useState(-1);
   const [passwdStatus, setPasswdStatus] = useState('');
 
   useEffect(() => {
-    if (isEmptyStatus(passwd, confirmPasswd)) {
+    if (isEmptyStatus(password, confirmPasswd)) {
       setPasswdStatus('');
-    } else if (isSamePasswd(passwd, confirmPasswd)) {
+    } else if (isSamePasswd(password, confirmPasswd)) {
       setPasswdStatus('비밀번호가 일치합니다.');
     } else {
       setPasswdStatus('비밀번호가 일치하지 않습니다.');
     }
-  }, [passwd, confirmPasswd]);
+  }, [password, confirmPasswd]);
 
   const onCheckHandler = (e) => {
     setIsAgree(() => e.target.checked);
+  };
+
+  const onSubmit = () => {
+    const requestData = {
+      userEmail,
+      password,
+      year,
+      userName,
+      pinCode: 1111,
+    };
+    console.log('verify request data', requestData);
   };
 
   return (
@@ -35,15 +47,16 @@ function SignUpBox() {
       <InputElement
         placeHolder="이메일"
         classNames="sign-up-box-email__input"
-        value={email}
-        setValue={setEmail}
+        value={userEmail}
+        setValue={setUserEmail}
+        type="text"
       />
-      <EmailStatus email={email} />
+      <EmailStatus email={userEmail} />
       <InputElement
         placeHolder="비밀번호"
         classNames="sign-up-box-password__input"
-        value={passwd}
-        setValue={setPasswd}
+        value={password}
+        setValue={setPassword}
         type="password"
       />
       <InputElement
@@ -57,15 +70,21 @@ function SignUpBox() {
       <InputElement
         placeHolder="이름"
         classNames="sign-up-box-name__input"
-        value={name}
-        setValue={setName}
+        value={userName}
+        setValue={setUserName}
+        type="text"
       />
-      <CardinalSelectBox classNames="sign-up-box-cardinal__select" />
+      <CardinalSelectBox
+        classNames="sign-up-box-cardinal__select"
+        setValue={setYear}
+      />
       <div className="sign-up-box--agree__container">
         <input type="checkbox" checked={isAgree} onChange={onCheckHandler} />
         <span>이용약관 및 개인정보 처리방침에 동의합니다.</span>
       </div>
-      <SignUpBtn />
+      <button className="sign-up__button" type="submit" onClick={onSubmit}>
+        가입하기
+      </button>
     </div>
   );
 }
