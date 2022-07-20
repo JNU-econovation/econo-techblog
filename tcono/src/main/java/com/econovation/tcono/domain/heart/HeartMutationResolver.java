16a95171c34927d62d6a86a81ad267048ff78584
kssumin <ksoomin25@gmail.com>
2022-07-20 21:14:09 +0900

    feat : 하트 누른 상태 여부 추가

--- tcono/src/main/java/com/econovation/tcono/domain/heart/HeartMutationResolver.java
+++ tcono/src/main/java/com/econovation/tcono/domain/heart/HeartMutationResolver.java
@@ -1,58 +1,44 @@
 package com.econovation.tcono.domain.heart;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.econovation.tcono.domain.post.Post;
 import com.econovation.tcono.domain.post.PostRepository;
-import com.econovation.tcono.domain.user.User;
-import com.econovation.tcono.domain.user.UserRepository;
+//import com.econovation.tcono.domain.user.User;
+//import com.econovation.tcono.domain.user.UserRepository;
 import com.econovation.tcono.web.dto.HeartRequestDto;
-import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class HeartMutationResolver implements GraphQLMutationResolver {
     private static final String NOT_FOUND_USER_MESSAGE = "존재하지 않은 회원입니다.";
-    private UserRepository userRepository;
+    private static final String NOT_FOUND_POST_MESSAGE = "해당 페이지가 존재하지 않습니다.";
+//    private UserRepository userRepository;
+    @Autowired
     private PostRepository postRepository;
+    @Autowired
     private HeartRepository heartRepository;
     private Post post;
 
-    /**
-     * 특정 User가 Post에 좋아요를 누름 -> true 반환
-     * @param
-     * @return boolean
-     */
-    @Transactional
-    public boolean isHeartByUser(HeartRequestDto heartAddRequestDto) {
-        // 존재하는 User인지 조사
-        User user = userRepository.findById(heartAddRequestDto.getUser().getId())
-                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_USER_MESSAGE));
-
-        // 존재하는 Post인지 조사
-        Post post = postRepository.findById(heartAddRequestDto.getPost().getId())
-                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_USER_MESSAGE));
-
-        //해당 User가 heart를 눌렀는지 조사
-        Optional<Heart> findHeart = heartRepository.findHeartByUserAndPost(user, post);
-
-        return findHeart.isPresent();
-    }
 
     @Transactional
     public int updateHeartState(HeartRequestDto heartRequestDto) {
-        //좋아요 누름 -> true 반환
-        boolean isHeart = isHeartByUser(heartRequestDto);
 
+        Post post=postRepository.findById(heartRequestDto.getPostId())
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_POST_MESSAGE));
         //좋아요 삭제
-        if (isHeart) {
-            heartRepository.deleteHeartByUserAndPost(heartRequestDto.getUser(),heartRequestDto.getPost());
+        if (heartRequestDto.getIsHeart()) {
+            heartRepository.delete(heartRequestDto.toEntity(post));
             return post.decreaseHearts();
         } else { // 좋아요 추가
-            heartRepository.save(heartRequestDto.toEntity());
+            heartRepository.save(heartRequestDto.toEntity(post));
             return post.increaseHearts();
         }
     }
