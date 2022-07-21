ccc3731141c86c6a00ac8ad907d0cbd87531797a
ryeeun <dmsdl7531@naver.com>
2022-07-21 17:38:05 +0900

    feat : apolloclient , apolloprovider 추가

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { ApolloProvider } from '@apollo/client/react';
 
 import './index.css';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://13.124.120.174/graphql/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </React.StrictMode>
   </BrowserRouter>,
 );
