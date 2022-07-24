2b1e11cb2df5493a0e0066f84381ffaced0b76b2
yunseong76039287 <dbsdltjd3701@naver.com>
2022-07-25 02:45:49 +0900

    feat : 비밀번호 찾기 페이지 레이아웃 구현

--- fe/src/FindPassword/FindPassword.jsx
+++ fe/src/FindPassword/FindPassword.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import CardinalSelectBox from '../components/CardinalSelectBox';
 import InputElement from '../components/InputElement';
+import './FindPassword.css';
 
 function FindPassword() {
   // eslint-disable-next-line no-unused-vars
@@ -15,29 +16,44 @@ function FindPassword() {
 
   return (
     <div className="find-password">
-      <h3 className="find-password__title">비밀번호 찾기</h3>
-      <InputElement placeHolder="이름" />
-      <CardinalSelectBox />
-      <button
-        className={`find-password__button ${
-          !isSendClick ? 'btn__active' : 'btn__inactive'
-        }`}
-        type="button"
-        disabled={isSendClick}
-        onClick={onClick}
-      >
-        코드 전송
-      </button>
-      {isSendClick ? (
-        <div>
-          <div>
-            <p>이메일 전송하기</p>
-            <button type="button">이메일 재전송</button>
+      <div className="find-password-box">
+        <div className="find-password__title">비밀번호 재설정</div>
+        <h4>비밀번호를 잊어버리셨나요?</h4>
+        <p>회원가입시 사용한 이메일을 입력하시면</p>
+        <p>이메일로 인증번호 6자리를 보내드립니다.</p>
+        <InputElement
+          placeHolder="이름"
+          classNames="find-password-name__input"
+        />
+        <CardinalSelectBox classNames="find-password-cardinal" />
+        <button
+          className={`find-password__button ${
+            !isSendClick ? 'btn__active' : 'btn__inactive'
+          }`}
+          type="button"
+          disabled={isSendClick}
+          onClick={onClick}
+        >
+          코드 전송
+        </button>
+        {isSendClick ? (
+          <div className="send-code--after">
+            <div className="email-resend__container">
+              <span>이메일을 받지 못했나요? </span>
+              <button type="button" className="email-resend__button">
+                이메일 재전송
+              </button>
+            </div>
+            <InputElement
+              placeHolder="인증번호 입력"
+              classNames="certification-number__input"
+            />
+            <button type="button" className="cerify__button">
+              인증완료
+            </button>
           </div>
-          <InputElement placeHolder="인증번호 입력" />
-          <button type="button">인증완료</button>
-        </div>
-      ) : undefined}
+        ) : undefined}
+      </div>
     </div>
   );
 }
