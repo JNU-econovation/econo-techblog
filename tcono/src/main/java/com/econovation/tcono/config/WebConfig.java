d5a1199027660530f42b92e2c12b08430d96b209
BlackBean99 <ymecca730135@gmail.com>
2022-07-24 19:10:45 +0900

    feat : initData 빈객체 생성 오류 해결

--- tcono/src/main/java/com/econovation/tcono/config/WebConfig.java
+++ tcono/src/main/java/com/econovation/tcono/config/WebConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -25,17 +24,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(2)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/api/user/**","/api/user","**/*.graphql","**/*.graphqls","/api/usernames/**","/api/confirm-email/**", "/api/login", "/api/logout","/api/find-email/**",
+                .excludePathPatterns("/", "/api/user/**","/api/user","**/*.graphql","**/*.graphqls","/api/usernames/**","/api/confirm-email/**", "/api/user/login", "/api/logout","/api/find-email/**",
                         "/css/**", "/*.ico", "/error");
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://54.180.29.85:3000").maxAge(3600); // 3600초 동안 preflight 결과를 캐시에 저장
-        registry.addMapping("/**").allowedOrigins("http://54.180.29.85:8080").maxAge(3600); // 3600초 동안 preflight 결과를 캐시에 저장
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedMethods("*")
                 .maxAge(3600)
                 .allowedHeaders("header1","header2")
                 .allowCredentials(true);
--- tcono/src/main/java/com/econovation/tcono/domain/user/UserRepository.java
+++ tcono/src/main/java/com/econovation/tcono/domain/user/UserRepository.java
@@ -1,16 +1,19 @@
 package com.econovation.tcono.domain.user;
 
 import com.econovation.tcono.domain.post.Post;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+
     @Query("SELECT u FROM User u WHERE u.userName = :userName")
     List<User> findByUserName(@Param("userName") String userName);
 
@@ -19,4 +22,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 
     @Query("SELECT u FROM User u WHERE u.pinCode = :pinCode")
     Optional<User> findUserByPinCode(String pinCode);
+
+    Page<User> findAll(Pageable pageable);
 }
