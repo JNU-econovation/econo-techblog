f6829d77d19e60f9c6c608c16cf3c304af527171
ryeeun <dmsdl7531@naver.com>
2022-07-24 15:44:24 +0900

    feat : post 페이지 쿼리문 작성 및 props 전달

--- fe/src/Post/Post.jsx
+++ fe/src/Post/Post.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-// import { useQuery } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
 import './css/Post.css';
 import PostInfo from './components/PostInfo';
@@ -8,46 +9,93 @@ import PostDesc from './components/PostDesc';
 import CommentList from './components/CommentList';
 import Liked from './components/Liked';
 
-/*
 const GET_POST = gql`
-  query getPost($postId: String!) {
+  query getPost($postId: Int!) {
     findPostByPostId(id: $postId) {
-      id
-      userId
+      postId
+      userName
       content
       title
-      official
+      mainCategoryNumber
+      categoryList
+      createdDate
       views
       hearts
-      mainCategory
     }
   }
 `;
-*/
+
+// 좋아요 상태 업데이트
+const SET_LIKED = gql`
+  mutation setLiked($heartRequest: heartRequestDto!) {
+    updateHeartState(heartRequestDto: $heartRequest)
+  }
+`;
+
+// 게시물 삭제
+const DELETE_POST = gql`
+  mutation delete($postId: Int!) {
+    deletePost(postId: $postId)
+  }
+`;
 
 const Post = function () {
+  const navigate = useNavigate();
   // const { id } = useParams();
-  // const result = useQuery(GET_POST, {
-  //   variables: {
-  //     postId: id,
-  //   },
-  // });
-  const [liked, setLiked] = useState(false);
+  const id = 1;
+  const { data, loading, error } = useQuery(GET_POST, {
+    variables: {
+      postId: id,
+    },
+  });
+  console.log(data);
+  const [updateHeartState] = useMutation(SET_LIKED, {
+    onCompleted: (result) => {
+      console.log(result);
+    },
+  });
+  const [deletePost] = useMutation(DELETE_POST, {
+    onCompleted: (result) => {
+      console.log(`게시물 삭제 결과 => ${result}`);
+    },
+  });
+  const [active, setActive] = useState(false);
+  const onClick = () => {
+    setActive(!active);
+    updateHeartState({
+      variables: {
+        heartRequest: {
+          postId: id,
+          userId: 1,
+          isHeart: active,
+        },
+      },
+    });
+  };
+  const onDelete = () => {
+    deletePost({
+      variables: {
+        postId: id,
+      },
+    });
+    navigate(-1); // 삭제 시 이전 페이지로 이동
+  };
+  if (loading) return <p>loading</p>;
+  if (error) return <p>ERR_CONNECTION_REFUSED</p>;
   return (
     <div className="post">
       <div className="post-middle">
-        <span className="post__title">
-          에코노베이션 멋알팀에서 에코노베이션 기술 블로그를 제작하였습니다.
-        </span>
+        <span className="post__title">{data.findPostByPostId.title}</span>
         <PostInfo
-          author="에코노베이션"
-          date="2022.05.04"
-          views="21"
-          hearts="21"
+          author={data.findPostByPostId.userName}
+          date={data.findPostByPostId.createdDate}
+          views={data.findPostByPostId.views}
+          hearts={data.findPostByPostId.hearts}
+          onDelete={onDelete}
         />
-        <Tags />
+        <Tags tags={data.findPostByPostId.categoryList} />
         <PostDesc />
-        <Liked liked={liked} setLiked={setLiked} />
+        <Liked active={active} setActive={onClick} />
       </div>
       <div className="post-partition" />
       <CommentList />
