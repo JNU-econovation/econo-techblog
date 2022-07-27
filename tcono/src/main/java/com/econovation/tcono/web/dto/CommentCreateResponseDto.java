50aa3938b77f267b2d4680f13899c82b56dbb08e
kssumin <ksoomin25@gmail.com>
2022-07-27 14:45:07 +0900

    feat : 댓글 생성 기능

--- tcono/src/main/java/com/econovation/tcono/web/dto/CommentCreateResponseDto.java
+++ tcono/src/main/java/com/econovation/tcono/web/dto/CommentCreateResponseDto.java
@@ -1,5 +1,7 @@
 package com.econovation.tcono.web.dto;
 
+import com.econovation.tcono.domain.comment.Comment;
+import com.econovation.tcono.domain.user.User;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -8,7 +10,7 @@ import lombok.Getter;
 @Getter
 public class CommentCreateResponseDto {
     private Long commentId;//comment Id 정보
-    private Long userId;
+    private String userName;
     private Long postId;
     private String content;
     private int depth;
@@ -17,13 +19,13 @@ public class CommentCreateResponseDto {
     private String createdDate;
 
     @Builder
-    public void CommentCreateRequestDto(Long commentId,Long userId, Long postId, String content, int depth, int group){
-        this.commentId=commentId;
-        this.userId=userId;
-        this.postId=postId;
-        this.content=content;
-        this.depth=depth;
-        this.group=group;
+    public CommentCreateResponseDto(Comment comment, User user){
+        this.commentId=comment.getCommentId();
+        this.userName=user.getUserName();
+        this.postId=comment.getPost().getId();
+        this.content= comment.getContent();;
+        this.depth=comment.getDepth();
+        this.group=comment.getGroup();
     }
 
 }
