653cfb8e5374ca0cf6febd87694990c3bf0af087
kssumin <ksoomin25@gmail.com>
2022-07-29 00:09:07 +0900

    feat : 특정 글의 댓글 목록 보기

--- tcono/src/main/java/com/econovation/tcono/domain/comment/CommentQueryResolver.java
+++ tcono/src/main/java/com/econovation/tcono/domain/comment/CommentQueryResolver.java
@@ -1,68 +1,33 @@
-//package com.econovation.tcono.domain.comment;
-//
-//import com.econovation.tcono.domain.post.Post;
-//import com.econovation.tcono.domain.post.PostRepository;
-//import com.econovation.tcono.domain.user.UserRepository;
-//import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-//public class CommentQueryResolver implements GraphQLQueryResolver {
-////    private static final String NOT_FOUND_USER_MESSAGE = "해당 유저는 존재하지 않습니다";
-////    private static final String NOT_FOUND_POST_MESSAGE = "해당 페이지는 존재하지 않습니다.";
-//    private static final String NOT_FOUND_POST_USER = "해당 댓글은 존재하지 않습니다.";
-//    private CommentRepository commentRepository;
-//    private UserRepository userRepository;
-//    private PostRepository postRepository;
-//
-//    @Transactional
-//    public Comment findCommentById(Long commentId) throws Exception {
-//        return commentRepository.findById(commentId)
-//                .orElseThrow(() -> new Exception("댓글이 없습니다."));
-//    }
-//
-//
-//    @Transactional
-//    public List<Comment> findAllComment() {
-//        return commentRepository.findAll();
-//    }
-//
-//    //== 비즈니스 로직 ==//
-//    public List<Comment> findRemovableList() {
-//
-//        List<Comment> result = new ArrayList<>();
-//
-//        Optional.ofNullable(this.parent).ifPresentOrElse(
-//
-//                parentComment ->{//대댓글인 경우 (부모가 존재하는 경우)
-//                    if( parentComment.isRemoved()&& parentComment.isAllChildRemoved()){
-//                        result.addAll(parentComment.getChildList());
-//                        result.add(parentComment);
-//                    }
-//                },
-//
-//                () -> {//댓글인 경우
-//                    if (isAllChildRemoved()) {
-//                        result.add(this);
-//                        result.addAll(this.getChildList());
-//                    }
-//                }
-//        );
-//
-//        return result;
-//    }
-//
-//
-//    //모든 자식 댓글이 삭제되었는지 판단
-//    private boolean isAllChildRemoved() {
-//        return getChildList().stream()
-//                .map(Comment::isRemoved)//지워졌는지 여부로 바꾼다
-//                .filter(isRemove -> !isRemove)//지워졌으면 true, 안지워졌으면 false이다. 따라서 filter에 걸러지는 것은 false인 녀석들이고, 있다면 false를 없다면 orElse를 통해 true를 반환한다.
-//                .findAny()//지워지지 않은게 하나라도 있다면 false를 반환
-//                .orElse(true);//모두 지워졌다면 true를 반환
-//
-//    }
-//
-//}
+package com.econovation.tcono.domain.comment;
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.econovation.tcono.domain.post.Post;
+import com.econovation.tcono.domain.post.PostRepository;
+import com.econovation.tcono.domain.user.UserRepository;
+import com.econovation.tcono.web.dto.CommentResponseDto;
+import com.econovation.tcono.web.dto.PostListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class CommentQueryResolver implements GraphQLQueryResolver {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
+
+    private static final String NOT_FOUND_POST_MESSAGE = "해당 글이 존재하지 않습니다.";
+
+    public List<CommentResponseDto> findCommentByPost(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_POST_MESSAGE));
+
+        List<Comment> comment = commentRepository.findCommentsByPost(post);
+        return comment.stream().map(x -> new CommentResponseDto(x, userRepository.findById(x.getUserId()).get()))
+                .collect(Collectors.toList());
+    }
+}
--- tcono/src/main/java/com/econovation/tcono/domain/comment/CommentRepository.java
+++ tcono/src/main/java/com/econovation/tcono/domain/comment/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.econovation.tcono.domain.comment;
 
+import com.econovation.tcono.domain.post.Post;
+import com.econovation.tcono.web.dto.CommentResponseDto;
+import com.econovation.tcono.web.dto.PostListResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +14,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     @Query("select count(c)from Comment c group by c.parent")
     Integer countByParent();
+
+    @Query("select c from Comment c where c.post=:post")
+    List<Comment> findCommentsByPost(@Param("post") Post post);
 }
