120e13329f7994ea51c159f6232c54c84a36f958
ryeeun <dmsdl7531@naver.com>
2022-07-29 00:56:52 +0900

    feat : post 삭제 쿼리 및 기능 추가

--- fe/src/Manager/components/UserInfo.jsx
+++ fe/src/Manager/components/UserInfo.jsx
@@ -1,15 +1,31 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable object-shorthand */
+/* eslint-disable no-shadow */
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { gql, useMutation } from '@apollo/client';
 
 import noImg from '../img/no_img.png';
 import '../css/UserInfo.css';
 import PostList from './PostList';
 
+// 게시물 삭제
+const DELETE_POST = gql`
+  mutation deletePost($postId: Int!) {
+    deletePost(postId: $postId)
+  }
+`;
+
 const UserInfo = function () {
   const { id } = useParams();
   const [user, setUser] = useState({});
   const [checkedList, setCheckedList] = useState([]);
+  const [deletePost] = useMutation(DELETE_POST, {
+    onCompleted: () => {
+      location.reload(); // 삭제 시 이전 페이지로 이동
+    },
+  });
   useEffect(() => {
     axios
       .get(`http://168.131.30.127:8080/api/user/${id}`)
@@ -24,13 +40,19 @@ const UserInfo = function () {
 
   const onChange = (checked, item) => {
     if (checked) {
-      setCheckedList([...checkedList, item]);
+      setCheckedList((checkedList) => [...checkedList, item]);
     } else if (!checked) {
       setCheckedList(checkedList.filter((el) => el !== item));
     }
   };
   const onDelete = () => {
-    console.log(checkedList);
+    checkedList.forEach((postId) => {
+      deletePost({
+        variables: {
+          postId: postId,
+        },
+      });
+    });
   };
 
   return (
