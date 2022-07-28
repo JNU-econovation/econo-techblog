6deba7fcf2f91f689bc985c78f45fa6863a4ac74
ryeeun <dmsdl7531@naver.com>
2022-07-28 15:10:05 +0900

    feat : post link 설정 및 param 추가

--- fe/src/Tech/components/Official.jsx
+++ fe/src/Tech/components/Official.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 // import { gql, useMutation } from '@apollo/client';
 
 import '../css/Official.css';
@@ -25,7 +26,7 @@ import Tags from '../../components/Tags';
 
 const result = [
   {
-    pid: 1,
+    postId: 1,
     author: '에코노베이션',
     title: 'slider1',
     desc: '안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.이전 글에서 말씀드렸듯이 Android 구름톡을 가지고 돌아왔습니다. 지난 2월, 구름톡 성과를 가지고 전사 발표까지 하게 됐는데요. 정말 영광이었습니다. 발표를 하고 나서 가장 공감받았던 부분은 바로 “정말 힘들었습니다” (한숨) 부분이었습니다. 안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.이전 글에서 말씀드렸듯이 Android 구름톡을 가지고 돌아왔습니다. 지난 2월, 구름톡 성과를 가지고 전사 발표까지 하게 됐는데요. 정말 영광이었습니다. 발표를 하고 나서 가장 공감받았던 부분은 바로 “정말 힘들었습니다” (한숨) 부분이었습니다.',
@@ -34,7 +35,7 @@ const result = [
     liked: 21,
   },
   {
-    pid: 2,
+    postId: 2,
     author: '에코노베이션',
     title: 'slider2',
     desc: '안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.이전 글에서 말씀드렸듯이 Android 구름톡을 가지고 돌아왔습니다. 지난 2월, 구름톡 성과를 가지고 전사 발표까지 하게 됐는데요. 정말 영광이었습니다. 발표를 하고 나서 가장 공감받았던 부분은 바로 “정말 힘들었습니다” (한숨) 부분이었습니다. 안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.이전 글에서 말씀드렸듯이 Android 구름톡을 가지고 돌아왔습니다. 지난 2월, 구름톡 성과를 가지고 전사 발표까지 하게 됐는데요. 정말 영광이었습니다. 발표를 하고 나서 가장 공감받았던 부분은 바로 “정말 힘들었습니다” (한숨) 부분이었습니다.',
@@ -43,7 +44,7 @@ const result = [
     liked: 21,
   },
   {
-    pid: 3,
+    postId: 3,
     author: '에코노베이션',
     title: 'slider3',
     desc: '안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.이전 글에서 말씀드렸듯이 Android 구름톡을 가지고 돌아왔습니다. 지난 2월, 구름톡 성과를 가지고 전사 발표까지 하게 됐는데요. 정말 영광이었습니다. 발표를 하고 나서 가장 공감받았던 부분은 바로 “정말 힘들었습니다” (한숨) 부분이었습니다. 안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.이전 글에서 말씀드렸듯이 Android 구름톡을 가지고 돌아왔습니다. 지난 2월, 구름톡 성과를 가지고 전사 발표까지 하게 됐는데요. 정말 영광이었습니다. 발표를 하고 나서 가장 공감받았던 부분은 바로 “정말 힘들었습니다” (한숨) 부분이었습니다.',
@@ -75,7 +76,7 @@ const Official = function () {
         }
         return 0;
       });
-    }, 2500);
+    }, 3500);
     return () => clearTimeout(timeoutId);
   });
   useEffect(() => {
@@ -86,39 +87,44 @@ const Official = function () {
   };
   return (
     <div className="official">
-      <div className="official-post">
-        <div className="official__img" />
-        <div className="official-info">
-          <div className="official-info-top">
-            <span className="official-title">{currentPost.title}</span>
-            <Tags tags="CSS, HTML" />
-          </div>
-          <div className="official-info-middle">
-            <span>{currentPost.desc}</span>
-          </div>
-          <div className="official-info-bottom">
-            <div className="official-info-bottom-left">
-              <img
-                src={noImg}
-                alt="no-img"
-                className="official-info-bottom-left__img"
-              />
-              <span className="official-info-bottom-left__span">
-                {currentPost.author}
-              </span>
+      <Link
+        to={`/post/${currentPost.postId}`}
+        style={{ textDecoration: 'none' }}
+      >
+        <div className="official-post">
+          <div className="official__img" />
+          <div className="official-info">
+            <div className="official-info-top">
+              <span className="official-title">{currentPost.title}</span>
+              <Tags tags="CSS, HTML" />
             </div>
-            <PostDetails
-              date="2022.05.04"
-              views={currentPost.looked}
-              hearts={currentPost.liked}
-            />
+            <div className="official-info-middle">
+              <span>{currentPost.desc}</span>
+            </div>
+            <div className="official-info-bottom">
+              <div className="official-info-bottom-left">
+                <img
+                  src={noImg}
+                  alt="no-img"
+                  className="official-info-bottom-left__img"
+                />
+                <span className="official-info-bottom-left__span">
+                  {currentPost.author}
+                </span>
+              </div>
+              <PostDetails
+                date="2022.05.04"
+                views={currentPost.looked}
+                hearts={currentPost.liked}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
       <div className="official-bottom">
         {result.map((post, index) => (
           <button
-            key={post.pid}
+            key={post.postId}
             type="button"
             onClick={() => onClick(index)}
             className={
