57fe740da06b82df81827dea583ee3ccbc707f21
blackbean99 <ymecca12@gmail.com>
2022-07-28 06:59:09 +0000

    Revert "feat : query Param"
    This reverts commit 4748c198c4579a37a3b6ec9d667418b1e6cec495.

--- tcono/src/main/java/com/econovation/tcono/config/WebConfig.java
+++ tcono/src/main/java/com/econovation/tcono/config/WebConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -30,11 +29,21 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/", "/api/user/**","/api/user","/api/file","**/*.graphql","**/*.graphqls","/api/usernames/**","/api/confirm-email/**", "/api/login", "/api/logout","/api/find-email/**",
                         "/css/**", "/*.ico", "/error","/api/file");
     }
+    @Bean
+    public FilterRegistrationBean<CorsFilter> CorsFilter(){
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        registrationBean.setOrder(1);
+        registrationBean.setName("first-filter");
+        return registrationBean;
+    }
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*");
+//                .allowedOrigins("http:localhost:8080")
+//                .allowedMethods("*")
+//                .maxAge(3000);
 ////                .allowCredentials(true);
 //    }
 }
