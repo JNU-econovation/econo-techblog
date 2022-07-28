387fa5ccabdcf4084ceb117dec305b35052a6d59
2yunseong <dbsdltjd3701@naver.com>
2022-07-29 00:31:01 +0900

    feat: Main Component 배치

--- fe/src/Main/Main.jsx
+++ fe/src/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import HomeBanner from '../HomeBanner/HomeBanner';
+import RecentPosts from '../RecentPosts/RecentPosts';
+import HomeOffical from './HomeOffical';
 
 const Main = function () {
   return (
     <div className="main-page">
       <HomeBanner />
+      <HomeOffical />
+      <RecentPosts />
     </div>
   );
 };
