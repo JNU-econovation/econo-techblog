4cab3c72112f65535b7fb46a8f8fb2641b568d45
2yunseong <dbsdltjd3701@naver.com>
2022-07-28 23:49:52 +0900

    feat: Home banner 완성

--- fe/src/HomeBanner/HomeBanner.jsx
+++ fe/src/HomeBanner/HomeBanner.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+
 import './HomeBanner.css';
+import facebook from './img/facebook.png';
+import insta from './img/insta.png';
+import youtube from './img/youtube.png';
+import blog from './img/blog.png';
 
 function HomeBanner() {
   return (
@@ -11,6 +16,29 @@ function HomeBanner() {
           에코노베이션 기술 블로그 T-econo입니다. 문제 해결, 에코노베이션의 행사
           등 에코노 베이션의 다양한 소식을 접할 수 있습니다.
         </p>
+      </div>
+      <div className="home-banner-button__container">
+        <a
+          className="home-banner__link"
+          href="https://www.instagram.com/cnu_econovation/"
+        >
+          <img src={insta} alt="banner button" />
+        </a>
+        <a
+          className="home-banner__link"
+          href="https://www.facebook.com/econo.jnu.ac.kr/"
+        >
+          <img src={facebook} alt="banner button" />
+        </a>
+        <a className="home-banner__link" href="https://medium.com/econovation">
+          <img src={blog} alt="banner button" />
+        </a>
+        <a
+          className="home-banner__link"
+          href="https://www.youtube.com/channel/UCepdmgc4w5EQIc1J0lqXVtA"
+        >
+          <img src={youtube} alt="banner button" />
+        </a>
       </div>
     </div>
   );
