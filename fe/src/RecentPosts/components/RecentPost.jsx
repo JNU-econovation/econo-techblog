4aefa906a8daeef31ea2510fbc46d6f66850ebcf
ryeeun <dmsdl7531@naver.com>
2022-07-29 01:13:48 +0900

    refactor : classname 변경

--- fe/src/RecentPosts/components/RecentPost.jsx
+++ fe/src/RecentPosts/components/RecentPost.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './RecentPost.css';
 
-const Post = function () {
+const RecentPost = function () {
   return (
-    <div className="post">
-      <div className="post__img" />
-      <div className="post-info">
-        <span className="post-title">
+    <div className="recent-post">
+      <div className="recent-post__img" />
+      <div className="recent-post-info">
+        <span className="recent-post-title">
           구름톡 업데이트 48시간 전, 그 치열함에
         </span>
-        <span className="post-desc">
+        <span className="recent-post-desc">
           안녕하세요. 우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는
-          채상아 입니다. 이전 글에서 말씀드렸듯이 Android
+          채상아 입니다. 이전 글에서 말씀드렸듯이 Android 안녕하세요.
+          우아한형제들에서 만화경 안드로이드 앱을 개발하고 있는 채상아 입니다.
+          이전 글에서 말씀드렸듯이 Android
         </span>
-        <div className="post-tags">
+        <div className="recent-post-tags">
           <span>#html </span>
           <span>#css </span>
         </div>
       </div>
-      <div className="post-writer">
+      <div className="recent-post-writer">
         <span className="writer-name">에코노베이션</span>
         <div className="writer-img" />
       </div>
@@ -26,4 +28,4 @@ const Post = function () {
   );
 };
 
-export default Post;
+export default RecentPost;
